25b094760e2e1da0637fc5efa20e7b07a996dcec
Konstantin Bukley <madundead@gmail.com>
2012-03-19 18:53:29 +0200

    added vim symlinks

--- scripts/symlinks/all.sh
+++ scripts/symlinks/all.sh
@@ -1,3 +1,6 @@
+# This file creates symlinks for config files in repo
+echo "Applying symlinks: "
+
 # gitconfig
 if [ ! -f ~/.gitconfig ];
 then
@@ -5,9 +8,19 @@ then
 	ln -s $ROOT/git/gitconfig ~/.gitconfig
 fi
 
-# TODO: make all config files gitconfig way
-# vim
-#echo "ln -s vim/_vimrc ~/.vimrc"
-#echo "ln -s vim/vimfiles ~/.vim"
+# vimrc
+if [ ! -f ~/.vimrc ];
+then
+	echo "ln -s $ROOT/vim/_vimrc ~/.vimrc"
+	ln -s $ROOT/vim/_vimrc ~/.vimrc
+fi
 
-# openbox
+# vimfiles
+if [ ! -f ~/.vim ];
+then
+	echo "ln -s $ROOT/vim/vimfiles ~/.vim"
+	ln -s $ROOT/vim/vimfiles ~/.vim
+fi
+
+# TODO: add openbox
+# TODO: add all settings files I use
